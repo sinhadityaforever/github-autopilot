48e298da8f0dd0d1ef4fa32b9b43799930c5a72a
Aditya Sinha <sinhadityaforever@gmail.com>
2023-05-30 18:19:43 +0530

    Original table styles

--- src/components/OriginalTable/OriginalTable.jsx
+++ src/components/OriginalTable/OriginalTable.jsx
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import './Table.css';
+import './OriginalTable.css';
 
 function createData(name, trackingId, date, status) {
 	return { name, trackingId, date, status };

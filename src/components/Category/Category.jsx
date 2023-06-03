7cfae90d23d514be31503946adba36799fe8c28a
Aditya Sinha <sinhadityaforever@gmail.com>
2023-06-03 12:03:13 +0530

    Minor Fixes

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,73 +2,83 @@ import React from 'react';
 import './Category.css';
 
 const BUDGET_ITEMS = [
-  {
-    itemName: 'Clothing',
-    budgeted: 100,
-    spent: 90,
-  },
-  {
-    itemName: 'Eating Out',
-    budgeted: 300,
-    spent: 59,
-  },
-  {
-    itemName: 'Video Games',
-    budgeted: 100,
-    spent: 200,
-  },
-  {
-    itemName: 'Video Games',
-    budgeted: 100,
-    spent: 200,
-  },
-  {
-    itemName: 'Video Games',
-    budgeted: 100,
-    spent: 200,
-  },
-  {
-    itemName: 'Video Games',
-    budgeted: 100,
-    spent: 200,
-  },
-]
+	{
+		itemName: 'Clothing',
+		budgeted: 100,
+		spent: 90
+	},
+	{
+		itemName: 'Eating Out',
+		budgeted: 300,
+		spent: 59
+	},
+	{
+		itemName: 'Video Games',
+		budgeted: 100,
+		spent: 200
+	},
+	{
+		itemName: 'Video Games',
+		budgeted: 100,
+		spent: 200
+	},
+	{
+		itemName: 'Video Games',
+		budgeted: 100,
+		spent: 200
+	},
+	{
+		itemName: 'Video Games',
+		budgeted: 100,
+		spent: 200
+	}
+];
 
 const BudgetItem = (props) => {
-  return (
-    <section className="budget-item-container">
-      <BudgetItemName {...props} />
-      <AllocationItem {...props} />
-      <ProgressBar {...props} />
-    </section>);
-}
+	return (
+		<section className="budget-item-container">
+			<BudgetItemName {...props} />
+			<AllocationItem {...props} />
+			<ProgressBar {...props} />
+		</section>
+	);
+};
 
-const BudgetItemName = ({itemName}) => {
-  return (
-    <div className="budget-item" title={itemName}>{itemName}</div>
-  )
-}
+const BudgetItemName = ({ itemName }) => {
+	return (
+		<div className="budget-item" title={itemName}>
+			{itemName}
+		</div>
+	);
+};
 
-const AllocationItem = ({spent, budgeted}) => {
-  return (
-    <div className="allocation">
-        <span className={"money " + ((spent > budgeted) ? 'over-budget' : '')}>₹
-{spent}</span> of <span class="money">₹
-{budgeted}</span>
-      </div>
-  );
-}
+const AllocationItem = ({ spent, budgeted }) => {
+	return (
+		<div className="allocation">
+			<span className={'money ' + (spent > budgeted ? 'over-budget' : '')}>
+				₹{spent}
+			</span>{' '}
+			of <span class="money">₹{budgeted}</span>
+		</div>
+	);
+};
 
-const ProgressBar = ({spent, budgeted}) => {
-  return (
-    <div className={"progress-bar " + ((spent > budgeted) ? 'over-budget' : '')}>
-      <progress max={budgeted} value={spent}>{spent}</progress>
-    </div>
-  )
-}
+const ProgressBar = ({ spent, budgeted }) => {
+	return (
+		<div className={'progress-bar ' + (spent > budgeted ? 'over-budget' : '')}>
+			<progress max={budgeted} value={spent}>
+				{spent}
+			</progress>
+		</div>
+	);
+};
 
-const Category = BUDGET_ITEMS.map(item => {
-  return <BudgetItem {...item} />
-})
+const Category = () => {
+	<div>
+		{BUDGET_ITEMS.map((item) => {
+			return <BudgetItem {...item} />;
+		})}
+	</div>;
+};
 
 export default Category;
--- src/pages/Budget/Budget.jsx
+++ src/pages/Budget/Budget.jsx
@@ -1,18 +1,16 @@
-import React,  { useState } from 'react';
+import React, { useState } from 'react';
 import Table from '../../components/Table/Table';
 import './Budget.css';
 import Cards from '../../components/Cards/Cards';
-import { Calculator, Fish } from "phosphor-react";
+import { Calculator, Fish } from 'phosphor-react';
 import Box from '../../components/Box/Box';
 import Setit from '../../components/Setit/Setit';
 import Overall from '../../components/Overall/Overall';
 import { useAppSelector } from '../../app/hooks';
 import Category from '../../components/Category/Category';
-
-
+import meterImg from '../../imgs/meter.png.png';
 
 function Budget() {
-
 	const [isClicked, setClicked] = useState(false);
 	const [isCross, setCross] = useState(false);
 
@@ -20,74 +18,73 @@ function Budget() {
 	// 	this.parentElement.style.display='none';
 	//   }
 
-	  function handleClick() {
+	function handleClick() {
 		setClicked(true);
-	  }
+	}
 
-	  function handleCross() {
+	function handleCross() {
 		setCross(true);
-	  }
+	}
 
-	  const transactionsData = useAppSelector(
+	const transactionsData = useAppSelector(
 		(state) => state.transactionState.transactions
-	    );
+	);
 
-	  const tableTransactionsData = [...transactionsData];
-	  tableTransactionsData.sort((a, b) => new Date(b.date) - new Date(a.date));
+	const tableTransactionsData = [...transactionsData];
+	tableTransactionsData.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-	  const recentTransactions = tableTransactionsData.slice(0, 9);
-
-
+	const recentTransactions = tableTransactionsData.slice(0, 9);
 
 	return (
-<div className="Budget">
-        
+		<div className="Budget">
 			<h1>Budget</h1>
 
 			<h3>Set Overall Budget</h3>
-			<input type="number"/>
-			<img src="../Images/meter.png" alt="Meter pic" />
-			
-             <h4>Set your category-wise budget:</h4>
-			
+			<input type="number" />
+			<img src={meterImg} alt="Meter pic" />
+
+			<h4>Set your category-wise budget:</h4>
+
 			<div className="grid-container">
-				<div>	
-				  <label for="category">Enter Category:</label>
-                 <select name="category" id="category" className="style">
-                   <option value="Business">Business</option>
-                   <option value="Investment">Investment</option>
-                   <option value="Salary" selected>Salary</option>
-                    <option value="Other">Other</option>
-                  </select>
-				</div>
-			
-			
-				  <div>
-			     <input type="number" placeholder="Current Budget" name="current" className="style"/>
-				 </div>
-
-				 <div>
-			    <input type="text" placeholder="New Budget" name="new" className="style" />
-			    </div>
-			 
 				<div>
-			     <button>+</button>
-			     <p className="inline">Add Category</p>
-				 </div>
+					<label for="category">Enter Category:</label>
+					<select name="category" id="category" className="style">
+						<option value="Business">Business</option>
+						<option value="Investment">Investment</option>
+						<option value="Salary" selected>
+							Salary
+						</option>
+						<option value="Other">Other</option>
+					</select>
+				</div>
 
-			 </div>
+				<div>
+					<input
+						type="number"
+						placeholder="Current Budget"
+						name="current"
+						className="style"
+					/>
+				</div>
 
-             <Category />
+				<div>
+					<input
+						type="text"
+						placeholder="New Budget"
+						name="new"
+						className="style"
+					/>
+				</div>
 
+				<div>
+					<button>+</button>
+					<p className="inline">Add Category</p>
+				</div>
+			</div>
 
-
-
-			 
-			 
-			
-	
-		
-    </div>);
+			<Category />
+		</div>
+	);
 }
 
 export default Budget;

e843e0aa9928154a389a374572cf01032f09b2b8
Aditya Sinha <sinhadityaforever@gmail.com>
2023-06-03 20:36:06 +0530

    fixed lowercase error

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import Sidebar from './components/Sidebar';
 import Budget from './pages/Budget/Budget';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Insights from './pages/Insights/Insights';
-import Profile from './pages/Profile/Profile';
+import Profile from './pages/profile/Profile';
 import LoginPage from './pages/Loginpage/Loginpage';
-
 
 function App() {
 	const [sidebarOption, setSidebarOption] = useState(0);
@@ -25,12 +24,10 @@ function App() {
 				return <Budget />;
 			case 2:
 				return <Insights />;
-				case 3:
+			case 3:
 				return <Profile />;
-				case 3:
+			case 3:
 				return <LoginPage />;
-			
-			
 
 			default:
 				return <Dashboard />;
